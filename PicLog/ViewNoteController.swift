8d55e3bd19e5f8b5a60bfc0f80244ef15fb9374c
rileymills <rtmills@deakin.edu.au>
2017-05-05 18:37:38 +1000

    Added swift file for the ViewController of view note page.

---
+++ PicLog/ViewNoteController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewNoteController.swift
+//  PicLog
+//
+//  Created by Riley Mills on 5/5/17.
+//  Copyright © 2017 Riley Mills & Daniel Catania. All rights reserved.
+//
+
+import UIKit
+
+class ViewNoteController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
