5084d03edbbc1325b84429edd46075fc5332f408
Kataniazz <Danielcatania@10-140-38-87.wifi-m.deakin.edu.au>
2017-05-17 15:57:39 +1000

    Camera and camera roll

--- PicLog/NewNoteController.swift
+++ PicLog/NewNoteController.swift
@@ -2,14 +2,18 @@
 //  ViewController2.swift
 //  PicLog
 //
-//  Created by Riley Mills on 25/4/17.
+//  Created by Riley Mills and Daniel Catania on 25/4/17.
 //  Copyright © 2017 Riley Mills & Daniel Catania. All rights reserved.
 //
 
 import UIKit
+import MobileCoreServices
 
 @available(iOS 10.0, *)
-class NewNoteController: UIViewController {
+class NewNoteController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var newMedia: Bool?
     
     @IBOutlet weak var noteTextField: UITextField!
     override func viewDidLoad() {
@@ -33,6 +37,61 @@ class NewNoteController: UIViewController {
         
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         let _ = navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func useCameraRoll(_ sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.savedPhotosAlbum) {
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType =
+                UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.mediaTypes = [kUTTypeImage as String]
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true,
+                         completion: nil)
+            newMedia = false
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+        
+        self.dismiss(animated: true, completion: nil)
+        
+        if mediaType.isEqual(to: kUTTypeImage as String) {
+            let image = info[UIImagePickerControllerOriginalImage]
+                as! UIImage
+            
+            imageView.image = image
+            
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self,
+                                               #selector(NewNoteController.image(image:didFinishSavingWithError:contextInfo:)), nil)
+            } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                // Code to support video here
+            }
+            
+        }
+    }
+    
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                                          message: "Failed to save image",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true,
+                         completion: nil)
+        }
     }
     
     
