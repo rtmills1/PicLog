1a0b24695cef1e703750c454208870d6d7a4fb3f
Kataniazz <Danielcatania@10-140-50-73.wifi-m.deakin.edu.au>
2017-05-18 13:46:22 +1000

    Internal documentation

--- PicLog/CameraController.swift
+++ PicLog/CameraController.swift
@@ -6,6 +6,29 @@
 //  Copyright © 2017 Riley Mills & Daniel Catania. All rights reserved.
 //
 
+/* This idea to sort in reverse is from a user RAFAEL GARCIA at http://www.appcoda.com/ios-programming-camera-iphone-app/
+ @IBAction func useCamera(_ sender: AnyObject) {
+ 
+ if UIImagePickerController.isSourceTypeAvailable(
+ UIImagePickerControllerSourceType.camera) {
+ 
+ let imagePicker = UIImagePickerController()
+ 
+ imagePicker.delegate = self
+ imagePicker.sourceType =
+ UIImagePickerControllerSourceType.camera
+ imagePicker.mediaTypes = [kUTTypeImage as String]
+ imagePicker.allowsEditing = false
+ 
+ self.present(imagePicker, animated: true,
+ completion: nil)
+ newMedia = true
+ }
+ }
+
+end RAFAEL GARCIA idea
+*/
+
 import UIKit
 import MobileCoreServices
 
--- PicLog/NewNoteController.swift
+++ PicLog/NewNoteController.swift
@@ -6,6 +6,72 @@
 //  Copyright © 2017 Riley Mills & Daniel Catania. All rights reserved.
 //
 
+/* This idea to sort in reverse is from a user RAFAEL GARCIA at http://www.appcoda.com/ios-programming-camera-iphone-app/
+ @IBAction func useCameraRoll(_ sender: AnyObject) {
+ 
+ if UIImagePickerController.isSourceTypeAvailable(
+ UIImagePickerControllerSourceType.savedPhotosAlbum) {
+ let imagePicker = UIImagePickerController()
+ 
+ imagePicker.delegate = self
+ imagePicker.sourceType =
+ UIImagePickerControllerSourceType.photoLibrary
+ imagePicker.mediaTypes = [kUTTypeImage as String]
+ imagePicker.allowsEditing = false
+ self.present(imagePicker, animated: true,
+ completion: nil)
+ newMedia = false
+ }
+ }
+ 
+and
+ 
+ func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+ 
+ let mediaType = info[UIImagePickerControllerMediaType] as! NSString
+ 
+ self.dismiss(animated: true, completion: nil)
+ 
+ if mediaType.isEqual(to: kUTTypeImage as String) {
+ let image = info[UIImagePickerControllerOriginalImage]
+ as! UIImage
+ 
+ imageView.image = image
+ 
+ if (newMedia == true) {
+ UIImageWriteToSavedPhotosAlbum(image, self,
+ #selector(NewNoteController.image(image:didFinishSavingWithError:contextInfo:)), nil)
+ } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+ // Code to support video here
+ }
+ 
+ }
+ }
+
+ 
+and
+ 
+ func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+ 
+ if error != nil {
+ let alert = UIAlertController(title: "Fail",
+ message: "Image failed",
+ preferredStyle: UIAlertControllerStyle.alert)
+ 
+ let cancelAction = UIAlertAction(title: "Okay",
+ style: .cancel, handler: nil)
+ 
+ alert.addAction(cancelAction)
+ self.present(alert, animated: true,
+ completion: nil)
+ }
+ }
+
+
+end RAFAEL GARCIA idea
+ */
+
+
 import UIKit
 import MobileCoreServices
 
